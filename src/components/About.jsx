ae6e1e6ced874e662001d4c1b274a4aaedc1f8a8
Hasan Huda <hasanhuda037@gmail.com>
2023-04-28 21:49:49 -0400

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in many languages and a
+        wide range of frameworks. I'm a quick learner and collaborate closely
+        with clients to create efficient, scalable, and user-friendly solutions
+        that solve real-world problems. Let's work together to bring your ideas
+        to life!
+        <br />
+        <br />
+        <p>
+          <strong>Languages:</strong> Javascript, Java, Python, HTML, CSS, C
+        </p>
+        <p>
+          <strong>Frameworks:</strong> React, Express, Flask,
+          AJAX, Django, Spring Boot, Tailwind, Material UI
+        </p>
+        <p>
+          <strong>Databases:</strong> SQL, MongoDB
+        </p>
+        <p>
+          <strong>Other Technologies:</strong> Git, AWS
+        </p>
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,304 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  sbu,
+  wcbhs,
+  codingdojo,
+  java,
+  sql,
+  python,
+  budgetapp,
+  bugtracker
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Python Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Java Developer",
+    icon: backend,
+  },
+  {
+    title: "Web Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript,
+  // },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "SQL",
+    icon: sql,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "William Cullen Bryant HS",
+    company_name: "High School Diploma Completed",
+    icon: wcbhs,
+    iconBg: "#FFFFFF",
+    date: "November 2015 - June 2019",
+    points: [
+      "HS Diploma in Math and Science Program",
+      "Top 3% of Graduating Class",
+      "Relevant Coursework: AP Computer Science Principles",
+    ],
+  },
+  {
+    title: "Stony Brook University",
+    company_name: "Towards BS in Computer Science",
+    icon: sbu,
+    iconBg: "#E6DEDD",
+    date: "Aug 2019 - May 2023",
+    points: [
+      "GPA: 3.1/4.0",
+      "Relevant Coursework: Object-Oriented Programming, Data Structures, Discrete Mathematics, Programming Abstractions, Theory of Computations, Analysis of Algorithms, System Fundamentals I, System Fundamentals (C Programming), Computer Networking, Scripting Languages, Software Security",
+      "Residential Safety Program (2019-2020)",
+      "Residential Hall Council Senator (2019-2020)",
+      "MSA Vice President (2021-2022)",
+    ],
+  },
+  {
+    title: "Coding Dojo",
+    company_name: "Software Developer Bootcamp",
+    icon: codingdojo,
+    iconBg: "#383E56",
+    date: "Nov 2022 - Mar 2023",
+    points: [
+      "Full Stack Developer Certificate",
+      "Immersive 4-month training program in Python, Java, Mongo, Express, React, Node js",
+      "Built CRUD applications in under 5 hours",
+      "Spent 1000+ hours on intensive coursework through all phases of project development of full-stack application including building, testing, debugging, and deploying",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Bug Tracker",
+    description:
+      "Web application for project managers to monitor software bugs across projects. User authentication & management, project management, and admin capabilities. Demo login to try it out!",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "materialUI",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "express",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: bugtracker,
+    front_code_link: "https://github.com/hasan-huda/bugtracker-frontend",
+    back_code_link: "https://github.com/hasan-huda/bugtracker-backend",
+    live_link: "https://hasan-bugtracker-app.onrender.com"
+  },
+  {
+    name: "Budget App",
+    description:
+      "Web application for users to manage their expenses, budgets, income and financial goals. User authentication implemented. Demo login to try it out!",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "java",
+        color: "green-text-gradient",
+      },
+      {
+        name: "javaSpring",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: budgetapp,
+    front_code_link: "https://github.com/hasan-huda/budget-frontend",
+    back_code_link: "https://github.com/hasan-huda/budget-backend",
+    live_link: "https://hasan-budget-app.onrender.com"
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    front_code_link: "https://github.com/",
+    back_code_link: "https://github.com/",
+    live_link: "https://github.com/"
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
