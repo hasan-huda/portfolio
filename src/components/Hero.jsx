8c4831186031b62ab2c5229b80e2e144c0c0cd2f
hasan-huda <hasanhuda037@gmail.com>
2023-05-30 21:42:57 -0400

    fixed grammar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Hasan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am Software Engineer based <br className='sm:block hidden' />
+            I am a Software Engineer based <br className='sm:block hidden' />
             in NYC
           </p>
         </div>
